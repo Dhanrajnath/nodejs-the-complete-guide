e0d0f763bb580be861fc55345be6bd7a5dc77bfb
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-03-23 17:14:27 +0530

    cart model implemented with crud operations

--- Section-11 Understanding Sequelize/app.js
+++ Section-11 Understanding Sequelize/app.js
@@ -5,6 +5,21 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const User = require("./models/user");
+const Product = require("./models/product");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+
+//Associations
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+
+User.hasMany(Product);
+User.hasOne(Cart);
+
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+
+Product.belongsToMany(Cart, { through: CartItem });
 
 const app = express();
 
@@ -17,14 +32,38 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  //---> middleware
+  User.findByPk(1) //---> incoming reqs are handled through middlewares
+    .then((user) => {
+      req.user = user;
+      next(); //---> calling next middleware
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 sequelize
-  .sync()
+  .sync() //sync({force: true})
   .then(() => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Dhanraj", email: "dhanraj@sequelize.com" });
+    }
+    // return Promise.resolve(user);
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => {
