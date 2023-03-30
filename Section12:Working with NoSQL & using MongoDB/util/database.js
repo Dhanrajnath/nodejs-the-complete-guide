81e25f8a95103ce5108ed9a063255e2af51339dc
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-03-30 11:55:40 +0530

    Fetching All Products

--- Section12:Working with NoSQL & using MongoDB/util/database.js
+++ Section12:Working with NoSQL & using MongoDB/util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect("mongodb://127.0.0.1:27017/")
+  MongoClient.connect("mongodb://localhost:27017/nodestudent")
     .then((client) => {
       console.log("Connected!");
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw new Error("No database found!");
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
