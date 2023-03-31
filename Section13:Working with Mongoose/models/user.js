1b34f7ed3ff93352f37b48879eec3450658d9856
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-03-31 11:01:55 +0530

    saving data through mongoose

--- Section13:Working with Mongoose/models/user.js
+++ Section13:Working with Mongoose/models/user.js
@@ -1,3 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
