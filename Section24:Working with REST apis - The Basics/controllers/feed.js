f380793a8f8ff2e0fde2220e56da7c4cf520f151
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-04 16:10:47 +0530

    create Post REST API

--- Section24:Working with REST apis - The Basics/controllers/feed.js
+++ Section24:Working with REST apis - The Basics/controllers/feed.js
@@ -1,3 +1,15 @@
 exports.getPosts = (req, res, next) => {
-  console.log(req, res);
+  res.status(200).json({
+    posts: [{ title: "first post", content: "this is the first post!!" }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //create post in db
+  res.status(201).json({
+    message: "Post created Successfully",
+    posts: { id: new Date().toISOString(), title: title, content: content },
+  });
 };
