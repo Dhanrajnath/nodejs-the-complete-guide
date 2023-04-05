20cea890073be27f8ad7510133e156f48fc76bae
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-05 13:05:16 +0530

    Signing Up Users

--- Section25:Working with REST apis- The Practical Application/BACKEND/controllers/auth.js
+++ Section25:Working with REST apis- The Practical Application/BACKEND/controllers/auth.js
@@ -1,6 +1,5 @@
-const path = require("path");
-
 const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 const fs = require("fs");
@@ -17,4 +16,25 @@ exports.signup = (req, res, next) => {
   const name = req.body.name;
   const password = req.body.password;
   //encrypt the password
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassowrd) => {
+      const user = new User({
+        email: email,
+        name: name,
+        password: hashedPassowrd,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({ message: "user creation success!!", userId: result._id });
+    })
+    .catch((error) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
