2ca1a1b02ca117f9411e6583495ffc377a429ca8
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-05 13:23:57 +0530

    Loggin In & Creating JSON Web Tokens (JWTs)

--- Section25:Working with REST apis- The Practical Application/BACKEND/controllers/auth.js
+++ Section25:Working with REST apis- The Practical Application/BACKEND/controllers/auth.js
@@ -1,8 +1,8 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const fs = require("fs");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -66,6 +66,17 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error;
       }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "supersecretkey",
+        { expiresIn: "1h" }
+      );
+      return res
+        .status(200)
+        .json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
