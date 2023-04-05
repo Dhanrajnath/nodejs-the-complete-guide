aca03fcd53479ccf60d3578d45ecb420b31f19be
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-05 12:00:08 +0530

    Uploading Images

--- Section25:Working with REST apis- The Practical Application/BACKEND/app.js
+++ Section25:Working with REST apis- The Practical Application/BACKEND/app.js
@@ -4,13 +4,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
@@ -27,7 +52,7 @@ app.use("/feed", feedRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err);
-  const statusCode = err.statusCode;
+  const statusCode = err.statusCode || 500;
   const message = err.message;
   res.status(statusCode).json({ message: message });
 });
